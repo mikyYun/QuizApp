17cdfc9ba9fc8e952df6bd61883a8573996a70d9
Moon Choi <choimy1991@gmail.com>
2022-05-07 15:24:25 -0700

    add quizzes seeds

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
+
+// MODULARIZE
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
 
