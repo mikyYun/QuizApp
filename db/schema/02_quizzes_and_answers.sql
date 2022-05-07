f101fac5b0d8a1bf097b23610f8755c9440e0920
mikyYun <mkyun2714@gmail.com>
2022-05-07 14:22:52 -0700

    update and new tables for users, quizzes_and_answers, attempts

---
+++ db/schema/02_quizzes_and_answers.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS quizzes_and_answers CASCADE;
+CREATE TABLE quizzes_and_answers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  creator VARCHAR(255) REFERENCES users(id),
+  quiz TEXT NOT NULL,
+  answer TEXT NOT NULL,
+  create_date DATE
+  is_public BOOLEAN NOT NULL,
+);
