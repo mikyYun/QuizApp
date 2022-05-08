49464d9894561ae90b71308df0cd08d1b74bfb4d
mikyYun <mkyun2714@gmail.com>
2022-05-07 18:59:29 -0700

    user_id 1 case

--- db/schema/02_quizzes_answers.sql
+++ db/schema/02_quizzes_answers.sql
@@ -1,8 +1,12 @@
 DROP TABLE IF EXISTS quizzes_and_answers CASCADE;
 CREATE TABLE quizzes_and_answers (
   id SERIAL PRIMARY KEY NOT NULL,
-  creator VARCHAR(255) REFERENCES users(id),
+  user_id REFERENCES users(id),
   quiz TEXT NOT NULL,
   answer TEXT NOT NULL,
   is_public BOOLEAN NOT NULL
 );
+
+insert into quizzes_and_answers (creator)
+VALUES (userid = primary key)
+user id = 1 -> program creator
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,5 @@
 -- Users table seeds here (Example)
+INSERT INTO users (user_name, password) VALUES ('creator', 'creator');
 INSERT INTO users (user_name, password) VALUES ('mike', 'mike');
 INSERT INTO users (user_name, password) VALUES ('will', 'will');
 INSERT INTO users (user_name, password) VALUES ('moon', 'moon');
