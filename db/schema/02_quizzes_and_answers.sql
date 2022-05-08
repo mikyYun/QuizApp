9edd5e1eb1233082aaa741bb955c0f6c9fa91b32
mikyYun <mkyun2714@gmail.com>
2022-05-07 18:35:57 -0700

    prefixed creator and is_public = true

--- db/schema/02_quizzes_and_answers.sql
+++ db/schema/02_quizzes_and_answers.sql
@@ -3,6 +3,6 @@ CREATE TABLE quizzes_and_answers (
   id SERIAL PRIMARY KEY NOT NULL,
   creator VARCHAR(255) REFERENCES users(id),
   quiz TEXT NOT NULL,
-  answer TEXT NOT NULL
-  is_public BOOLEAN NOT NULL,
+  answer TEXT NOT NULL,
+  is_public BOOLEAN NOT NULL
 );
