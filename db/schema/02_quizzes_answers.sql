6d000e0d3f80c02920e6ba98e2316e743fc7ed05
mikyYun <mkyun2714@gmail.com>
2022-05-07 21:22:35 -0700

    not needed one. ignore

--- db/schema/02_quizzes_answers.sql
+++ db/schema/02_quizzes_answers.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS quizzes_and_answers CASCADE;
 CREATE TABLE quizzes_and_answers (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id REFERENCES users(id),
+  -- user_id REFERENCES users(id),
   quiz TEXT NOT NULL,
   answer TEXT NOT NULL,
   is_public BOOLEAN NOT NULL
