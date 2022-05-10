bf513a8532750a3a4929d431b53e9160b8205949
Moon Choi <choimy1991@gmail.com>
2022-05-09 19:31:53 -0700

    GET, POST clean up

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -32,10 +32,8 @@ module.exports = (db) => {
   // ================== GET ==================== //
 
   router.get("/", (req, res) => {
-    // .redirect()
-    // const user_name = req.session.user_name
     const user_name = req.session.user_name;
-    // const user = 
+    // const user =
     getUserByName(user_name)
       .then((user) => {
         // res.cookie('user_id', user.id);
@@ -52,155 +50,128 @@ module.exports = (db) => {
             console.error(e);
             res.send(e);
           });
-
-        // getAllPublicQuiz()
-        //   .then((quizzes) => { // quiz == res.rows
-        //     const user = {}
-        //     const templateVars = {
-        //       user,
-        //       quizzes
-        //     };
-        //     res.render("quizzes", templateVars);
-        //   })
-        //   .catch((e) => {
-        //     console.error(e);
-        //     res.send(e);
-        //   });
-
-
       });
-    });
-    router.get("/private", (req, res) => {
-      const user_name = req.session.user_name;
-      getUserByName(user_name)
-        .then((user) => {
-          getAllPrivateQuiz()
-            .then((obj) => { // quizzes == res.rows [{quiz}, {quiz}, {quiz}, {} ] arr[0]
-              const templateVars = {
-                user,
-                obj
-              };
-              res.render("quiz_private", templateVars);
-            })
-            .catch((e) => {
-              console.error(e);
-              res.send(e);
-            });
-        });
-    });
+  });
 
-    router.get("/create", (req, res) => {
-      console.log("ROUTER/GET/CREATE");
-      const user_name = req.session.user_name;
-      getUserByName(user_name)
-        .then((user) => {
-          const templateVars = {
-            user
-          };
-          res.render("quiz_create", templateVars);
-        })
-        .catch((e) => {
-          console.error(e);
-          res.send(e);
-        });
-    });
+  router.get("/private", (req, res) => {
+    const user_name = req.session.user_name;
+    getUserByName(user_name)
+      .then((user) => {
+        getAllPrivateQuiz()
+          .then((obj) => { // quizzes == res.rows [{quiz}, {quiz}, {quiz}, {} ] arr[0]
+            const templateVars = {
+              user,
+              obj
+            };
+            res.render("quiz_private", templateVars);
+          })
+          .catch((e) => {
+            console.error(e);
+            res.send(e);
+          });
+      });
+  });
 
-    router.get("/result", (req, res) => {
-      console.log('ROUTER/GET/RESULT');
-      const user_name = req.session.user_name;
-      console.log(user_name);
-      getUserByName(user_name)
-        .then((user) => {
-          const templateVars = {
-            user
-          };
-          res.render("quiz_result", templateVars);
-        });
-    });
-
-    router.get("/show", (req, res) => {
-      console.log('ROUTER/GET/SHOW');
-
-      const quizURL = req.params.shortURL;
-      const userID = req.session.user_id;
-    });
-
-    // handling individual quiz page
-    router.get("/:quizID", (req, res) => {
-      const quizID = req.params.quizID;
-      const user_name = req.session.user_name;
-      getUserByName(user_name)
-        .then((user) => {
-          getPublicQuizID(quizID)
-            .then((quiz) => {
-              const oneQuiz = quiz[0];
-              const oneQuestion = quiz[0].question;
-
-              const templateVars = {
-                user,
-                oneQuiz: oneQuiz,
-                oneQuestion: oneQuestion
-              };
-
-              res.render("quiz_show", templateVars);
-            });
-        });
-    });
-    // const userID = req.session.user_id;
-
-
-    router.get("/create", (req, res) => {
-      const user_name = req.session.user_name;
-      getUserByName(user_name)
-        .then((user) => {
-          const templateVars = {
-            user
-          };
-          res.render("quiz_create", templateVars);
-        });
-    });
-
-    // ================== POST ==================== //
-    router.post("/create", (req, res) => {
-      // const quizURL = generateRandomString(); //abcde.
-      console.log(req.body);
-      const question = req.body.questionInput;
-      const answer = req.body.answerInput;
-
-      // getUserByName(user)
-
-      addPrivateQuiz({ question, answer, user_id: 1 })
-        .then((quiz) => {
-          res.send(quiz);
-        })
-        .catch((e) => {
-          console.error(e);
-          res.send(e);
-        });
-    });
-
-    // ================== GARY ==================== //
-    /*
-    router.get("/", (req, res) => {
-      res.render("quizzes");
-  
-  
-    res.render("quizzes", {}); //templateVars
-  
-    let query = `SELECT * FROM quizzes`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const quizzes = data.rows;
-        ///template vars and render instead of res.render
-        // res.render("", {res.row})
+  router.get("/create", (req, res) => {
+    console.log("ROUTER/GET/CREATE");
+    const user_name = req.session.user_name;
+    getUserByName(user_name)
+      .then((user) => {
+        const templateVars = {
+          user
+        };
+        res.render("quiz_create", templateVars);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((e) => {
+        console.error(e);
+        res.send(e);
       });
+  });
+
+  router.get("/result", (req, res) => {
+    console.log('ROUTER/GET/RESULT');
+    const user_name = req.session.user_name;
+    console.log(user_name);
+    getUserByName(user_name)
+      .then((user) => {
+        const templateVars = {
+          user
+        };
+        res.render("quiz_result", templateVars);
+      });
+  });
+
+  // handling individual quiz page
+  router.get("/:quizID", (req, res) => {
+    const quizID = req.params.quizID;
+    const user_name = req.session.user_name;
+    getUserByName(user_name)
+      .then((user) => {
+        getPublicQuizID(quizID)
+          .then((quiz) => {
+            const oneQuiz = quiz[0];
+            const oneQuestion = quiz[0].question;
+
+            const templateVars = {
+              user,
+              oneQuiz: oneQuiz,
+              oneQuestion: oneQuestion
+            };
+            res.render("quiz_show", templateVars);
+          });
+      });
+  });
+
+  router.get("/create", (req, res) => {
+    const user_name = req.session.user_name;
+    getUserByName(user_name)
+      .then((user) => {
+        const templateVars = {
+          user
+        };
+        res.render("quiz_create", templateVars);
+      });
+  });
+
+  // ================== POST ==================== //
+
+  router.post("/create", (req, res) => {
+    console.log(req.body);
+    const question = req.body.questionInput;
+    const answer = req.body.answerInput;
+
+    addPrivateQuiz({ question, answer, user_id: 1 })
+      .then((quiz) => {
+        res.send(quiz);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.send(e);
+      });
+  });
+
+  // ================== GARY ==================== //
+  /*
+  router.get("/", (req, res) => {
+    res.render("quizzes");
+
+
+  res.render("quizzes", {}); //templateVars
+
+  let query = `SELECT * FROM quizzes`;
+  console.log(query);
+  db.query(query)
+    .then(data => {
+      const quizzes = data.rows;
+      ///template vars and render instead of res.render
+      // res.render("", {res.row})
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
     });
-    */
-    return router;
-  };
+  });
+  */
+  return router;
+};
