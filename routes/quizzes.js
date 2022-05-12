5b5b9b445a4105bdcf606cd6fabdb03e8a789b03
Moon Choi <choimy1991@gmail.com>
2022-05-12 11:17:03 -0700

    delete comments

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -133,7 +133,7 @@ module.exports = (db) => {
     const user_id = req.session.user_id;
     const user = { user_name, user_id };
     console.log("req.params", req.params);
-    console.log("is_quizID", quizID); //WHY IS THIS UNDEFINED?
+    console.log("is_quizID", quizID);
     getPublicQuizID(quizID)
       .then((quiz) => {
         const oneQuiz = quiz[0];
@@ -178,7 +178,7 @@ module.exports = (db) => {
     const { quizID, private } = req.body;
     console.log("REQ.BODY", req.body);
     const userAnswer = req.body.userAnswer; //it's a string.
-    // console.log('check-quiz why is this undefined?', quizID);// ok
+    // console.log('QUIZ ID is: ', quizID);// ok
     // const quizID = req.params.quizID;
     // console.log("TEST", quizID); // ok
     const user_name = req.session.user_name;
