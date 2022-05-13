353e913675ad26f2e930d7a1297c9c4e13488b5d
Moon Choi <choimy1991@gmail.com>
2022-05-12 20:33:29 -0700

    clean up helpers.js

--- helpers.js
+++ helpers.js
@@ -3,19 +3,6 @@ const generateRandomString = function () {
   return str;
 };
 
-const urlsForUser = function (id, urlDB) {
-  let userURLs = {}; //shortURL, longURL, userID
-  for (const url in urlDB) {
-    if (urlDB[url]["userID"] === id) {
-      userURLs[url] = urlDB[url]; //we need key-value pair.
-      //key: url   //value: urlDB[url]
-    }
-  }
-  // console.log("OBJECT", userURLs);
-  return userURLs;
-};
-
 module.exports = {
   generateRandomString,
-  urlsForUser
 };
